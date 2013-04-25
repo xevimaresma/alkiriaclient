ef2db23aae1fc93182bb4cf2fe4a4d92545efa11
inane <inane.ensemble@gmail.com>
2013-04-25 10:17:37 +0200

    nueva ventana settings

---
+++ src/com/example/WhatsGroupDesign/Settings.java
@@ -0,0 +1,18 @@
+package com.example.WhatsGroupDesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Oscar
+ * Date: 25/04/13
+ * Time: 09:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Settings extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
