af5019ff3406a9d409bed2b9e72cf6a31083bc06
inane <inane.ensemble@gmail.com>
2013-05-03 22:22:28 +0200

    Diseño nuevas actividades y pantallas

--- src/com/example/WhatsGroupDesign/MyActivity.java
+++ src/com/example/WhatsGroupDesign/MyActivity.java
@@ -6,32 +6,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MyActivity extends Activity implements View.OnClickListener {
+
+    private Button b1,b2;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        b1 = (Button)findViewById(R.id.btnCreate);
+        b2 = (Button)findViewById(R.id.btnAccount);
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+    }
 
-        Button b1 = (Button)findViewById(R.id.buttonCreate);
-        Button b2 = (Button)findViewById(R.id.buttonAccount);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MyActivity.this, Settings.class);
-                startActivity(i);
-            }
-        });
-        /*b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MyActivity.this, Contact.class);
-                startActivity(i);
-            }
-        });*/
-
+    @Override
+    public void onClick(View v) {
+        if(v == b1){
+            Intent i = new Intent(MyActivity.this, Dialog.class);
+            startActivity(i);
+            finish();
+        }
+        else if(v == b2){
+            Intent j = new Intent(MyActivity.this, Settings.class);
+            startActivity(j);
+            finish();
+        }
 
     }
 }
--- src/com/example/WhatsGroupDesign/Settings.java
+++ src/com/example/WhatsGroupDesign/Settings.java
@@ -1,14 +1,32 @@
 package com.example.WhatsGroupDesign;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.WhatsGroupDesign.R.layout.settings;
 
 
-public class Settings extends Activity {
+
+public class Settings extends Activity implements View.OnClickListener {
+    private Button btnCancel;
+    //private Button btnCreate;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(settings);
+        btnCancel = (Button)findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(this);
 
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(v == btnCancel){
+            Intent k = new Intent(Settings.this, MyActivity.class);
+            startActivity(k);
+        }
     }
 }
