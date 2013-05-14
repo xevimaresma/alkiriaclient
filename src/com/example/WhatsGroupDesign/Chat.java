79f57f3b757c8cad59fb5ecae91cd869c35f2443
Miquel Serrabassa <girona@gmail.com>
2013-05-14 07:58:24 +0200

    Comentarios in-code en Contact.java i Chat.java

--- src/com/example/WhatsGroupDesign/Chat.java
+++ src/com/example/WhatsGroupDesign/Chat.java
@@ -41,18 +41,26 @@ import static com.example.WhatsGroupDesign.R.layout.chat;
 
 public class Chat extends Activity {
 	
+	// Definim variables, primer un handler per poder actualitzar els missatges quan s'actualitzi
 	private Handler uiCallback;
 
+	// Els elements de la interfície
     private EditText message;
     private Button enviar;
     private String mailContacte;
     private MsgSender missEnvia;
     LinearLayout la;
     
+    // I les dades de socket, per tenir-ne només un i poder rebre de forma centralitzada.
     byte[] buf = new byte[260];
     DatagramSocket socket;
     DatagramPacket packet;
     
+    // Mètodes AsyncTask per comunicació UDP.
+    
+    // Aquest cas és el d'enviament de missatge. Fa l'enviament al servidor, espera la resposta, i
+    // si aquesta és OK, esborra el text de l'input, i en cas contrari dóna l'error o mostra la
+    // resposta del servidor.
     protected class CommunicationTaskUDP extends AsyncTask<byte[], Void, String>{        
         public static final int PORT = 9876;
         
@@ -83,26 +91,8 @@ public class Chat extends Activity {
     	}	
     }
     
-    protected class CommunicationTaskUDPByte extends AsyncTask<byte[], Void, Void>{        
-        public static final int PORT = 9876;
-        
-        @Override
-        protected void onPostExecute(Void param) {        	
-        }
-        
-    	@Override
-    	protected Void doInBackground(byte[]... params) {
-    		byte[] dadesRebudes;
-            try {               
-            	missEnvia.enviamentUDPByte(params[0],socket);
-            } catch (Exception ex) {
-                Logger.getLogger(CommunicationTaskUDPByte.class.getName()).log(Level.SEVERE, null, ex);
-                dadesRebudes=null;
-            } 
-            return null;
-    	}	
-    }
-
+    // En aquest cas, el mètode no fa res OnPostExecute, només llança el missatge via UDP
+    // perquè un thread que obrim a l'onCreate el "caci" després.
     protected class CommunicationTaskUDPByteActualitza extends AsyncTask<byte[], Void, Void>{        
         public static final int PORT = 9876;
         
@@ -115,7 +105,7 @@ public class Chat extends Activity {
             try {
                 missEnvia.enviamentUDPByte(params[0],socket);                
             } catch (Exception ex) {
-                Logger.getLogger(CommunicationTaskUDPByte.class.getName()).log(Level.SEVERE, null, ex);                
+                Logger.getLogger(CommunicationTaskUDPByteActualitza.class.getName()).log(Level.SEVERE, null, ex);                
             } 
             return null;
     	}	
@@ -124,24 +114,32 @@ public class Chat extends Activity {
     
     public void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(chat);
+        setContentView(chat); 
         Intent intent = getIntent();
+        // Instanciem el LinearLayout que és on hi haurà els missatges
         la = (LinearLayout)findViewById(R.id.messageListlayout);        
         la.setOrientation(LinearLayout.VERTICAL);
-        
+        // I creem un handler per poder actualitzar fàcilment.
         uiCallback = new Handler () {
-            public void handleMessage (Message msg) {            	
+            public void handleMessage (Message msg) {         
+            	// Rebrà un missatge (msg), instanciarà un nou TextView de text negre
                 TextView tv = new TextView(getApplicationContext());
                 tv.setTextColor(Color.BLACK);
+                // I li assignarà les dades del missatge, partint el text a ||END|| i prenent només la part que hi hagi davant,
+                // així no tenim espais en blanc usats per omplir l'array de bytes.
                 tv.setText(msg.getData().getString("msg").split("\\|\\|END\\|\\|")[0]);
+                // Unnim la vista TextView al LinearLayout
                 la.addView(tv);
             }
         };
         
+        // Prenem la variable que ens venia amb el mail del contacte
         this.mailContacte=intent.getStringExtra("contacto");
+        // La mostrem en un toast
         Toast.makeText(getApplicationContext(),mailContacte, Toast.LENGTH_LONG).show();
         message = (EditText)findViewById(R.id.message);
         enviar = (Button)findViewById(R.id.sendMessage);
+        // I comencem: connectem el socket al port 9876.
         try {	        
 	  		socket = new DatagramSocket(9876);
 	  		socket.setBroadcast(true);
@@ -150,6 +148,7 @@ public class Chat extends Activity {
         	System.out.println("ERROR "+e.toString());
         }
         
+        // Ara obrim un thread per "escoltar" el socket UDP
        Thread t = new Thread() {
     	    public void run() {           	 
            	 try {           	
@@ -161,7 +160,9 @@ public class Chat extends Activity {
                     socket.receive(packet);
                     ByteBuffer buffer = ByteBuffer.wrap(packet.getData());
                     int tipus = buffer.getInt();
+                    // Quan arribin dades prenem el primer enter. Si és 3, ens arriba llista de missatges
                     if (tipus==3) {
+                    	// per tant, prenem les dades i les guardem
                     	byte[] arrtoken = new byte[64];
                         buffer.get(arrtoken);
                         byte[] arrdesti = new byte[64];
@@ -172,7 +173,7 @@ public class Chat extends Activity {
                         buffer.get(arrmsg);  
                         int horaLliura=buffer.getInt();
                         int horaEnvia=buffer.getInt();                        
-                        
+                        // Convertim bytes a cadenes
                         String token=new String(arrtoken).trim();
                         String remitent=new String(arrremitent).trim();
                         String mistxt=new String(arrmsg).trim();
@@ -180,13 +181,18 @@ public class Chat extends Activity {
                         String missatgeS="";
                         Encryption encripta=new Encryption();
                         encripta.setClau(token);
+                        // Intentem desencriptar el missatge
                         try {
                         	encripta.decrypt(arrmsg);
                         	missatgeS=encripta.getMsgDesencriptat().trim();
                         } catch (Exception e) { } 
+                        // I com que arriben tots els missatges junts, només mostrem els del remitent
+                        // que ens ha arribat amb l'intent
                         if (remitent.equals(mailContacte)) {
 	                        String dadestxt="<"+remitent+"> "+missatgeS+".";
-	                    	Log.d("UDP",dadestxt);
+	                    	//Log.d("UDP",dadestxt);
+	                        
+	                        // I l'escribim amb el handler que havíem creat abans
 	                    	Bundle b = new Bundle();
 	                    	b.putString("msg", dadestxt);
 	                    	Message msg = new Message();
@@ -201,12 +207,14 @@ public class Chat extends Activity {
            	 }   
     	    }     	               
     	};
-    	
+    	// Engeguem el thread
     	t.start();
     	
+    	// Prenem el token de les SharedPreferences
     	SharedPreferences prefs = getSharedPreferences("alkiria", MODE_PRIVATE);
         String token= prefs.getString("tokenAlkiria", null);
         missEnvia=new MsgSender("",token);
+        // I enviem un missatge al servidor sol·licitant la llista de missatges rebuts, per mostrar-los d'entrada.
         try {
         	byte[] missEnviaByte=missEnvia.enviaMsg(token,mailContacte,3);                	
         	CommunicationTaskUDPByteActualitza c = new CommunicationTaskUDPByteActualitza();
@@ -216,20 +224,23 @@ public class Chat extends Activity {
         	e.printStackTrace();
         }        
     	    	
-        
+        // Botó enviar missatge 
         enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(v == enviar){
+                	// Passem el missatge a text, prenem el token per xifrar-lo i el generem
                     String msg = message.getText().toString();
                     SharedPreferences prefs = getSharedPreferences("alkiria", MODE_PRIVATE);
                     String token= prefs.getString("tokenAlkiria", null);
                     if (token==null) { token="0000"; }
                     missEnvia=new MsgSender(msg,token);
                     try {
+                    	// Finalment preparem l'enviament i el fem amb una AsyncTask
                     	byte[] missEnviaByte=missEnvia.enviaMsg(token,mailContacte,2);
                     	CommunicationTaskUDP c = new CommunicationTaskUDP();
                         c.execute(missEnviaByte);
+                        // I directament afegim a la UI via handler el missatge enviat
                         Bundle b = new Bundle();                      
                     	b.putString("msg", "-> "+missEnvia.getMissatge());
                     	Message msgPosa = new Message();
@@ -254,12 +265,14 @@ public class Chat extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.undo:
+            	// Aquest botó torna enrere
                 Intent i = new Intent(Chat.this, Contact.class);
                 startActivity(i);
                 socket.close();
                 finish();
                 break;
             case R.id.add:
+            	// I aquest sol·licita actualització, com quan hem entrat per primer cop a l'Activity
             	SharedPreferences prefs = getSharedPreferences("alkiria", MODE_PRIVATE);
                 String token= prefs.getString("tokenAlkiria", null);
                 missEnvia=new MsgSender("",token);
--- src/com/example/WhatsGroupDesign/Contact.java
+++ src/com/example/WhatsGroupDesign/Contact.java
@@ -21,10 +21,14 @@ import android.widget.*;
 
 public class Contact extends Activity implements View.OnClickListener{
 
+	// Definim les variables de la classe
     private Button btnSalir;
+    // El diàleg de càrrega de contactes
     ProgressDialog pdu;
+    // I la llista de contactes predefinida, per les proves i la demo ens facilita trobar amb qui comunicar-nos 
     private String[] contactos = {"xevimaresma@gmail.com","prova","miquelserrabassa@gmail.com","xevi@cdmon.com","inane.ensemble@gmail.com","brossa@ramosiso.com"};
     
+    // Mètode per localitzar els mails dels nostres contactes. Sol·licita el llistat, mira si tenen correu i si és així, el posa en un ArrayList que retorna
     public ArrayList<String> getNameEmailDetails() {
         ArrayList<String> names = new ArrayList<String>();
         ContentResolver cr = getContentResolver();
@@ -37,7 +41,6 @@ public class Contact extends Activity implements View.OnClickListener{
                         ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ?", 
                                 new String[]{id}, null); 
                 while (cur1.moveToNext()) { 
-                    //to get the contact names
                     String name=cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
                     Log.e("Name :", name);
                     String email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
@@ -51,7 +54,12 @@ public class Contact extends Activity implements View.OnClickListener{
         }
         return names;
     }
-    
+
+    /* Com que la càrrega anterior pot ser molt lenta si tenim molts contactes,
+     * vàrem optar per fer-la córrer en segon pla perquè sinó semblava que es bloquegés la UI.
+     * D'aquesta manera, el processdialog es mostra i no queda tant parat -ni Android demana si
+     * volem tancar l'app.
+     */
     protected class carregaContactes extends AsyncTask<Void, Void, ArrayList<String>> {
 
         @Override
@@ -67,6 +75,7 @@ public class Contact extends Activity implements View.OnClickListener{
 
         @Override
         protected void onPostExecute(ArrayList<String> nomsContactes) {
+        	// Rebem ja els contactes, així que assignem l'ArrayList a un adaptador, l'adaptador al ListView de contactes        	// 
         	setContentView(R.layout.contact);            
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(Contact.this, android.R.layout.simple_list_item_1,nomsContactes);
             ListView lv = (ListView) findViewById(R.id.listaContactos);
@@ -75,6 +84,7 @@ public class Contact extends Activity implements View.OnClickListener{
             lv.setFocusable(false);
             lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             	@Override
+            	// I si algú clica una opció, obrim l'activity Chat passant-li una variable "contacto" amb el mail del contacte.
             	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                     Intent n = new Intent(Contact.this, Chat.class);
                 	n.putExtra("contacto",(String) parent.getItemAtPosition(position));
@@ -89,6 +99,7 @@ public class Contact extends Activity implements View.OnClickListener{
         
 
     public void onCreate(Bundle savedInstanceState) {
+    	// A l'obrir, mostrarem el ListView amb els valors de demo.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact);
         btnSalir = (Button)findViewById(R.id.salirContact);
@@ -112,6 +123,8 @@ public class Contact extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         if(v == btnSalir){
+        	// El botó de Logout carrega les SharedPreferences d'Alkiria i les esborra per després tornar
+        	// a MyActivity, la pantalla de login.
         	SharedPreferences.Editor editor= getSharedPreferences("alkiria", MODE_PRIVATE).edit();
     		editor.clear();    		
     		editor.commit();
@@ -130,6 +143,7 @@ public class Contact extends Activity implements View.OnClickListener{
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.searchContact:
+            	// Aquest botó temporalment serveix per fer logout.
             	SharedPreferences.Editor editor= getSharedPreferences("alkiria", MODE_PRIVATE).edit();
         		editor.clear();    		
         		editor.commit();
@@ -143,6 +157,7 @@ public class Contact extends Activity implements View.OnClickListener{
                 pdu = ProgressDialog.show(Contact.this,"Updating contacts","Retrieving contact data from your phone. Please be patient.",true);
                 //Determina si el usuario puede cancelar la operaciÃ³n
                 pdu.setCancelable(false);
+                // Càrrega de contactes del mòbil, de moment en "demo" amb diàleg de càrrega.
                 carregaContactes tCarregaContactes = new carregaContactes();
                 tCarregaContactes.execute();
         }
